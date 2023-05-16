582b0a8fbe451604e7fc63e89254e7b92cafe680
Ryan S <ryansouza@usp.br>
2023-05-16 00:23:05 -0300

    style: refactor nav css

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -48,52 +48,67 @@ nav {
   background-color: #5597b4;
   box-shadow: 0 1px 1px #ccc;
   border-radius: 2px;
-}
 
-nav a {
-  display: inline-block;
-  padding: 18px 30px;
-  color: #fff !important;
-  font-weight: bold;
-  font-size: 16px;
-  text-decoration: none !important;
-  line-height: 1;
-  text-transform: uppercase;
-  background-color: transparent;
+  a {
+    display: inline-block;
+    padding: 18px 30px;
+    color: #fff !important;
+    font-weight: bold;
+    font-size: 16px;
+    text-decoration: none !important;
+    line-height: 1;
+    text-transform: uppercase;
+    background-color: transparent;
 
-  -webkit-transition: background-color 0.25s;
-  -moz-transition: background-color 0.25s;
-  transition: background-color 0.25s;
-}
+    transition: background-color 0.25s;
 
-nav a:first-child {
-  border-radius: 2px 0 0 2px;
-}
+    &:first-child {
+      border-radius: 2px 0 0 2px;
+    }
 
-nav a:last-child {
-  border-radius: 0 2px 2px 0;
-}
+    &:last-child {
+      border-radius: 0 2px 2px 0;
+    }
+  }
 
-nav.home .home,
-nav.projects .projects,
-nav.services .services,
-nav.contact .contact {
-  background-color: #e35885;
+  &.home {
+    a.home {
+      background-color: #e35885;
+    }
+  }
+
+  &.projects {
+    a.projects {
+      background-color: #e35885;
+    }
+  }
+
+  &.services {
+    a.services {
+      background-color: #e35885;
+    }
+  }
+
+  &.contact {
+    a.contact {
+      background-color: #e35885;
+    }
+  }
 }
 
 p {
   font-size: 22px;
   font-weight: bold;
   color: #7d9098;
-}
 
-p b {
-  color: #ffffff;
-  display: inline-block;
-  padding: 5px 10px;
-  background-color: #c4d7e0;
-  border-radius: 2px;
-  text-transform: uppercase;
-  font-size: 18px;
+  b {
+    color: #ffffff;
+    display: inline-block;
+    padding: 5px 10px;
+    background-color: #c4d7e0;
+    border-radius: 2px;
+    text-transform: uppercase;
+    font-size: 18px;
+  }
 }
 </style>
